e570546d739c21e08f50c2c3e1318c44489c1809
Juan Li Puma <juanlipuma94@gmail.com>
2020-05-05 09:52:43 -0300

    Add argument parser for TP1 ex 1

--- lib/bioinformatics/tp1/ex1.rb
+++ lib/bioinformatics/tp1/ex1.rb
@@ -1,18 +1,21 @@
 # frozen_string_literal: true
 
 require 'bio'
+require_relative 'options'
 
-module Bioinformatics::TP1
-  # Read a nucleotide sequence in GenBank format, convert it to aminoacid sequence and print it out in FASTA
-  # format.
-  # Adapted from <gems-root>/bio-2.0.1/doc/Tutorial.rd.html
-  class Ex1
-    ff = Bio::FlatFile.new(Bio::GenBank, File.open('NM_002049.gbk'))
-    ff.each_entry do |entry|
-      definition = "#{entry.accession} #{entry.definition}"
-      sequence = entry.to_biosequence
-      sequence.aa # Convert to amino acid (AA) sequence TODO check this, we also need to check open reading frames
-      puts sequence.to_fasta(definition, 60)
+module Bioinformatics
+  module TP1
+    # Read a nucleotide sequence in GenBank format, convert it to aminoacid sequence and print it out in FASTA
+    # format.
+    # Adapted from <gems-root>/bio-2.0.1/doc/Tutorial.rd.html
+    class Ex1
+      args = Options.new.parse
+      ff = Bio::FlatFile.new(Bio::GenBank, args[:in])
+      ff.each_entry do |entry|
+        definition = "#{entry.accession} #{entry.definition}"
+        sequence = entry.to_biosequence
+        args[:out].write sequence.to_fasta(definition, 60)
+      end
     end
   end
 end
---
+++ lib/bioinformatics/tp1/options.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+require 'optparse'
+
+module Bioinformatics
+  module TP1
+    class Options
+      DEFAULT_INPUT_FILE = File.join __dir__, 'NM_002049.gbk'
+
+      def parse
+        args = { in: DEFAULT_INPUT_FILE, out: nil }
+
+        OptionParser.new do |parser|
+          parser.banner = 'Usage: ex1.rb [options]'
+
+          parser.on('-i IN', '--in IN', "Input file. Defaults to #{DEFAULT_INPUT_FILE}.") do |x|
+            args[:in] = x
+          end
+          parser.on('-o OUT', '--out OUT', 'Output file. Defaults to STDOUT.') do |x|
+            args[:out] = x
+          end
+        end.parse!
+
+        # Convert in/out to files
+        args[:in] = File.open(args[:in], 'r')
+        args[:out] = args[:out].nil? ? STDOUT : File.open(args[:out], 'w')
+
+        args
+      end
+    end
+  end
+end
