857cc44062d77adf0683b48f016a8a2378a3675d
Juan Li Puma <juanlipuma94@gmail.com>
2020-05-05 12:17:19 -0300

    Support calling ex 1 externally

--- lib/bioinformatics/tp1/ex1/ex1.rb
+++ lib/bioinformatics/tp1/ex1/ex1.rb
@@ -10,12 +10,29 @@ module Bioinformatics
       # format.
       # Adapted from <gems-root>/bio-2.0.1/doc/Tutorial.rd.html
       class Ex1
-        args = Options.new.parse
-        ff = Bio::FlatFile.new(Bio::GenBank, args[:in])
-        ff.each_entry do |entry|
-          definition = "#{entry.accession} #{entry.definition}"
-          sequence = entry.to_biosequence
-          args[:out].write sequence.to_fasta(definition, 70)
+        # Automatically run if running class directly. Based on https://stackoverflow.com/a/2249332/2333689
+        run if __FILE__ == $PROGRAM_NAME
+
+        def run
+          args = Options.new.parse
+          ff = Bio::FlatFile.new(Bio::GenBank, args[:in])
+          ff.each_entry do |entry|
+            definition = "#{entry.accession} #{entry.definition}"
+            sequence = entry.to_biosequence
+            args[:out].write sequence.to_fasta(definition, 70)
+          end
+        end
+
+        # Equivalent to run but takes parameters and returns value instead of using in/out arguments
+        def run_return(input = File.open(Options::DEFAULT_INPUT_FILE))
+          ff = Bio::FlatFile.new(Bio::GenBank, input)
+          fasta_entries = ff.map do |entry|
+            definition = "#{entry.accession} #{entry.definition}"
+            sequence = entry.to_biosequence
+            sequence.to_fasta(definition, 70)
+          end
+
+          fasta_entries.join "\n"
         end
       end
     end
